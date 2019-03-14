d5ecd47cb60818d48b990c6aa6047fb22b0de23b
Gennadiy <genaydzhan70@gmail.com>
2019-03-14 19:37:54 +0600

    Make a basic User model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,8 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example name", email: "user@example.com")
+    @user = User.new(name: "Example name", email: "user@example.com",
+      password: "foobar", password_confirmation: "foobar")
   end
 
   test "should be valid" do
@@ -26,6 +27,48 @@ class UserTest < ActiveSupport::TestCase
 
   test "email should not be too long" do
     @user.email = "a" * 244 + "@example.com"
+    assert_not @user.valid?
+  end
+
+  test "email validation should accept valid adresses" do
+    emails = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+      first.last@foo.jp alice+bob@baz.cn genaydzhan70@gmail.com]
+    emails.each do |email|
+      @user.email = email
+      assert @user.valid?, "#{email.inspect} should be valid"
+    end
+  end
+
+  test "email validation should not accept invalid adresses" do
+    emails = %w[user@example,com user_at_foo.org user.name@example
+      foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+    emails.each do |email|
+      @user.email = email
+      assert_not @user.valid?, "#{email.inspect} should be invalid"
+    end
+  end
+
+  test "email should be unique" do
+    user_duplicate = @user.dup
+    user_duplicate.email = @user.email.upcase
+    @user.save
+    assert_not user_duplicate.valid?
+  end
+
+  test "email adress should be saved as lower-case" do
+    mixed_case_email = "Foo@ExAMPle.CoM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
+  test "password should be present" do
+    @user.password = @user.password_confirmation = " "*6
+    assert_not @user.valid?
+  end
+
+  test "password should not be too short" do
+    @user.password = @user.password_confirmation = "a"*5
     assert_not @user.valid?
   end
 end
