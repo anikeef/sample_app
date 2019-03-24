0ea0bf68f62875c76dfb8bac9816ffcd50010119
Gennadiy <genaydzhan70@gmail.com>
2019-03-24 17:58:54 +0600

    Implement log_in and log_out with remember token

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,7 @@ class SessionsController < ApplicationController
     user = User.find_by(email: session_params[:email].downcase)
     if user && user.authenticate(session_params[:password])
       log_in user
+      remember user 
       redirect_to user
     else
       flash.now[:danger] = "Invalid password or email"
