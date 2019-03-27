edcc504e82e68d2a121bbcfca9f1d93a6bc9e289
Gennadiy <genaydzhan70@gmail.com>
2019-03-27 21:10:45 +0600

    Create admin column

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,8 +37,19 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     assert_redirected_to root_url
   end
 
-  test "should redirect inde if not logged in" do
+  test "should redirect index if not logged in" do
     get users_path
     assert_redirected_to login_url
+  end
+
+  test "should not allow the admin attribute to be edited via the web" do
+    log_in_as(@other_user)
+    assert_not @other_user.admin?
+    patch user_path(@other_user), params: {user: {
+      password: "foobar",
+      password_confirmation: "foobar",
+      admin: true
+    }}
+    assert_not @other_user.reload.admin?
   end
 end
