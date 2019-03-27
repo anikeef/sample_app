e026fd29e451b129f0ede53d1766894f696aef1d
Gennadiy <genaydzhan70@gmail.com>
2019-03-27 16:27:37 +0600

    Create User#index

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,14 +1,26 @@
 require 'test_helper'
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-  test "layout links" do
+  def setup
+    @user = users(:example)
+  end
+
+  test "home page links" do
     get root_path
     assert_template "static_pages/home"
     assert_select "a[href=?]", root_path, count: 2
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", help_path
     assert_select "a[href=?]", contact_path
-    get signup_path
-    assert_select "title", full_title("Sign up")
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", signup_path
+
+    log_in_as(@user)
+    follow_redirect!
+    assert_select "a[href=?]", login_path, false
+    assert_select "a[href=?]", users_path
+    assert_select "a[href=?]", user_path(@user)
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", edit_user_path(@user)
   end
 end
