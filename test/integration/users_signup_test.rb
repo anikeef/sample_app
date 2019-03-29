3e15d19545a9847e1556f6b153477b63040dbf90
Gennadiy <genaydzhan70@gmail.com>
2019-03-29 13:41:32 +0600

    Add test for account activation

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_select "form[action='/users']"
@@ -25,11 +29,26 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
         email: "foo@bar.com",
         password: "foobar",
         password_confirmation: "foobar"
-        }}
-      assert flash.any?
-      follow_redirect!
-      # assert_template "users/show"
-      # assert is_logged_in?
+      }}
     end
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    user = assigns(:user)
+    assert_not user.activated?
+    #Try to log in before activation
+    log_in_as(user)
+    assert_not is_logged_in?
+    #Invalid activation link
+    get edit_account_activation_path("Invalid", email: user.email)
+    assert_not is_logged_in?
+    #Invalid email
+    get edit_account_activation_path(user.activation_token, email: "ivalid@email.com")
+    assert_not is_logged_in?
+    #Valid activation
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert is_logged_in?
+    assert user.reload.activated?
+    assert flash.any?
+    follow_redirect!
+    assert_template "users/show"
   end
 end
